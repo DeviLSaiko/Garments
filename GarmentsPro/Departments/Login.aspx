8d04fda7c80bb737f1f053580879e1a7efeea042
Admin <Admin@DESKTOP-U2A2GFO>
2022-07-05 09:19:28 +0530

    Assets added

---
+++ GarmentsPro/Departments/Login.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Garments_Pro.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+    <script src="../Assets/Bootstrap/bootstrap.bundle.min5.js"></script>
+    <link href="../Assets/Bootstrap/bootstrap.min5.css" rel="stylesheet" />
+    <link href="../Assets/SweetAlert/sweetalert.css" rel="stylesheet" />
+    <script src="../Assets/SweetAlert/sweetalert.min.js"></script>
+    <style>
+        body  {
+             
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="mt-5">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <label>UserName</label>
+                    <asp:TextBox ID="txtUserName" CssClass="form-control" runat="server" Height="35px" Width="288px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2"
+                        ControlToValidate="txtUserName"
+                        Text=" This Field Cannot be Empty"
+                        ForeColor="red"
+                        Font-Size="Small"
+                        runat="server"></asp:RequiredFieldValidator>
+                    <br />
+                    <label>Department</label>
+                    <asp:DropDownList ID="ddDepartments" CssClass="form-select" Height="35px" Width="288px" runat="server"></asp:DropDownList>
+                    <br />
+                    <label>Password</label>
+                    <asp:TextBox ID="txtPassword" TextMode="Password" CssClass="form-control" runat="server" Height="35px" Width="288px"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1"
+                        ControlToValidate="txtPassword"
+                        Text=" This Field Cannot be Empty"
+                        ForeColor="red"
+                        Font-Size="Small"
+                        runat="server"></asp:RequiredFieldValidator>
+                    <br />
+                    <div class="d-grid">
+                        <asp:Button ID="BtnCreate" runat="server" OnClick="BtnCreate_Click" Text="Login" CssClass="btn btn-dark" Width="207px" Height="39px" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GarmentsPro/Departments/Login.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Garments_Pro
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        String MyCon = @"Data Source =.; Initial Catalog = GarmentsPro; Integrated Security =SSPI ";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataTable MyTable = new DataTable();
+
+                using (SqlConnection Sqlconnection = new SqlConnection(MyCon))
+                {
+                    SqlDataAdapter myada = new SqlDataAdapter("select * from Departments ", Sqlconnection);
+                    myada.Fill(MyTable);
+                }
+                ddDepartments.DataTextField = "DepName";
+                ddDepartments.DataValueField = "DepName";
+                ddDepartments.DataSource = MyTable;
+                ddDepartments.DataBind();
+            }
+            Session.RemoveAll();
+            Session.Remove("UserName");
+            Session.Remove("UserName");
+        }
+
+        private void LoginDetails(string Username, string Password)
+        {
+
+            //LoginDetails MyObj = new LoginDetails();
+
+            //MyObj.IsAuthUser = false;
+
+            using (SqlConnection Sqlconnection = new SqlConnection(MyCon))
+            {
+                SqlDataAdapter myada = new SqlDataAdapter("select * from Departments ", Sqlconnection);
+                //myada.Fill(MyTable);
+            }
+
+        }
+
+        //public static class Authentication
+        //{
+        //    static void SignIn( HttpContextBase context,string emailAddress,bool rememberMe, LoggedInUser user != null)
+        //    {
+        //        var cookie = FormsAuthentication.GetAuthCookie(
+        //            emailAddress.ToLower(),
+        //            rememberMe);
+
+
+        //        var oldTicket = FormsAuthentication.Decrypt(cookie.Value);
+        //        var newTicket = new FormsAuthenticationTicket(
+        //            oldTicket.Version,
+        //            oldTicket.Name,
+        //            oldTicket.IssueDate,
+        //            oldTicket.Expiration,
+        //            oldTicket.IsPersistent,
+        //            JsonConvert.SerializeObject(user ?? new LoggedInUser()));
+
+        //        cookie.Value = FormsAuthentication.Encrypt(newTicket);
+
+        //        context.Response.Cookies.Add(cookie);
+        //    }
+
+        //    static void SignOut(HttpContextBase context)
+        //    {
+        //        FormsAuthentication.SignOut();
+        //    }
+
+        //    static LoggedInUser GetLoggedInUser()
+        //    {
+        //        if (HttpContext.Current.User?.Identity?.Name != null) ;
+
+        //        return new LoggedInUser();
+        //    }
+        //}
+
+
+
+ //if (HttpContext.Current.User?.Identity?.Name != null && HttpContext.Current.User?.Identity is FormsIdentity identity)
+ //           return JsonConvert.DeserializeObject<LoggedInUser>(identity.Ticket.UserData);
+
+
+
+
+        protected void BtnCreate_Click(object sender, EventArgs e)
+        {
+            SqlConnection Sqlconnection = new SqlConnection(MyCon);
+
+            Sqlconnection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT UserName , Password , Department from UserInfo  where UserName=@UserName and Password=@Password", Sqlconnection);
+            cmd.Parameters.AddWithValue("@UserName", txtUserName.Text.Trim());
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+            cmd.Parameters.AddWithValue("@Department", ddDepartments.Text.Trim());
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Session["Department"] = dr.GetValue(0).ToString();
+
+                    if (ddDepartments.Text == "Fabric Formation")
+                    {
+                        Response.Redirect("~/Yarn_Formation/Dashboard.aspx");
+                    }
+                    else if (dr.GetValue(2).ToString() == "Fabric Formation")
+                    {
+                        Response.Redirect("~/Fabric_Formation/Dashboard.aspx");
+                    }
+                    else if (dr.GetValue(2).ToString() == "Wet Processing")
+                    {
+                        Response.Redirect("~/Wet_Processing/Dashboard.aspx");
+                    }
+                    else if (dr.GetValue(2).ToString() == "Fabrication")
+                    {
+                        Response.Redirect("~/Fabrication/Dashboard.aspx");
+                    }
+                    else if (dr.GetValue(2).ToString() == "Finished Goods")
+                    {
+                        Response.Redirect("~/Finished_Goods/Dashboard.aspx");
+                    }
+                }
+            }
+            else
+            {
+                Response.Redirect("~/Login.aspx");
+
+                txtUserName.Text = string.Empty;
+                txtPassword.Text = string.Empty;
+                txtUserName.Focus();
+            }
+        }
+    }
+}
+
+
