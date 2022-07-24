6545a45c7a5bff09afee686a5c9d8ee9e3ef8357
Admin <Admin@DESKTOP-U2A2GFO>
2022-07-24 05:35:44 +0530

    Db

--- GarmentsPro/DB/Yarn_Formation.sql
+++ GarmentsPro/DB/Yarn_Formation.sql
@@ -2,4 +2,4 @@
  TRUNCATE TABLE Status
 
   insert into Status (OID , Yarn_Formation , Fabric_Formation , Wet_Processing ,Fabrication ,Finished_Goods ) 
-  values ('ORD1' ,'Yet to Start' , 'Yet to Start' ,'Yet to Start' , 'Yet to Start' , 'Yet to Start'  )
+  values ('ORD1' ,'Yet to Start' , 'Yet to Start' ,'Yet to Start' , 'Yet to Start' , 'Yet to Start')
